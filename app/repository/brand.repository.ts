930b4ee8b960a42d16c1a48f4905ed09859dacb7
ryangst <ryangst.hire@gmail.com>
2024-12-14 13:54:10 -0300

    feat: add api calls

--- app/repository/brand.repository.ts
+++ app/repository/brand.repository.ts
@@ -14,4 +14,30 @@ const getModelsByBrand = async (brandId: string) => {
   return response.data
 }
 
-export const brandRepository = { getBrands, getModelsByBrand }
+const getVehicleModels = async (brandId: string, modelId: string) => {
+  const response = await api.apisauce.get<ModelResponse>(
+    `/carros/marcas/${brandId}/modelos/${modelId}/anos`,
+  )
+  return response.data
+}
+
+type VehiclePrice = {
+  TipoVeiculo: number
+  Valor: string
+  Marca: string
+  Modelo: string
+  AnoModelo: number
+  Combustivel: string
+  CodigoFipe: string
+  MesReferencia: string
+  SiglaCombustivel: string
+}
+
+const getVehiclePrice = async (brandId: string, modelId: string, year: string) => {
+  const response = await api.apisauce.get<VehiclePrice>(
+    `/carros/marcas/${brandId}/modelos/${modelId}/anos/${year}`,
+  )
+  return response.data
+}
+
+export const brandRepository = { getBrands, getModelsByBrand, getVehicleModels, getVehiclePrice }
